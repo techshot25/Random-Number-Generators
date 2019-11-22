e47f3fd65f3ec542a30b7f5696e148e1a0efbf8a
Ali Shannon <alishanoon@hotmail.com>
2019-11-22 14:32:19 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from matplotlib import pyplot as plt
+# %% generate a random normal distribution
 
-from math import log
+from matplotlib import pyplot as plt
+from math import log, erf
 
 def lcg(seed=4321, m=7829, a=378, c=2310):
     running_val = seed
@@ -8,13 +9,38 @@ def lcg(seed=4321, m=7829, a=378, c=2310):
     while running_val not in nums:
         nums.add(running_val)
         running_val = (running_val * a + c) % m
-    nums = list(nums)
-    return [num/max(nums) for num in nums]
+        yield running_val / (m - 1)
 
-inverse_cdf = lambda x: -log(1 / x - 1) if (1 > x > 0) else 0
-
-x = lcg(4232)
-y = [inverse_cdf(val) for val in x]
-
-plt.hist(y, bins=20)
+uniform_random_samples = [num for num in lcg()]
+plt.hist(uniform_random_samples, bins=50)
 plt.show()
+
+# %%
+
+def generate(inverse_cdf=None, seed=4321, m=7829, a=378, c=2310):
+    running_val = seed
+    nums = set()
+    while running_val not in nums:
+        nums.add(running_val)
+        running_val = (running_val * a + c) % m
+        if callable(inverse_cdf):
+            yield inverse_cdf(running_val / (m - 1))
+        else:
+            yield running_val / (m - 1)
+
+normal = lambda x: -log(1 / x - 1) if (1 > x > 0) else 0
+
+li = [val for val in generate(inverse_cdf=normal)]
+plt.hist(li, bins=50)
+plt.show()
+
+# %%
+
+exponential = lambda x: -log(x) if (1 > x > 0) else 0
+
+li = [val for val in generate(inverse_cdf=exponential)]
+plt.hist(li, bins=50)
+plt.show()
+
+
+# %%
